7c50435341f4afe8a3b87c27a55eb503b6719442
sufiabdul <muhammad.abdulrahim@jacks.sdstate.edu>
2014-10-14 22:52:59 -0500

    driver check udp

--- src/edu/sdstate/eastweb/prototype/MainDriver.java
+++ src/edu/sdstate/eastweb/prototype/MainDriver.java
@@ -1,24 +1,47 @@
 package edu.sdstate.eastweb.prototype;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 import javax.xml.parsers.ParserConfigurationException;
+
 import org.xml.sax.SAXException;
 
 public class MainDriver {
 
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+    public static void main(final String[] args) throws Exception {
 
         String temp = MetaData.GetInstance().IndicesMetaData;
 
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                // run stuff on back ground
+                try {
+
+                    Server(args);
+
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                System.out.println("RECEIVED: ");
             }
         };
 
         new Thread(r).start();
 
+        Thread.sleep(5000);
+        Client(args);
     }
+
+
+}
 }
 
