f92da8b118e40a5ea6cbeb4010e589ee66b65fce
michael.devos <michael.devos.jacks@gmail.com>
2015-02-02 09:09:23 -0600

    Removed extra constructors from summary.SummaryData

--- src/version2/prototype/summary/SummaryData.java
+++ src/version2/prototype/summary/SummaryData.java
@@ -27,7 +27,7 @@ public class SummaryData {
 
     /**
      * <p>Accepts values for all inputs.</p>
-     * 
+     *
      * @param inRaster - Type: File[] - A File object for each DataDate.<br/>
      * Example:  <code>{@link #version2.prototype.DirectoryLayout.getIndexMetadata(ProjectInfo, String, DataDate, String)}<br/>
      * getIndexMetadata(mProject, mIndex, sDate, zone.getShapeFile())</code>
@@ -69,100 +69,8 @@ public class SummaryData {
         zoneField = zone;
         summaries = new SummariesCollection(summarySingletonNames);
         this.inDate = inDate;
-        this.hrsPerInputData = hrsPerInputData;
-        this.hrsPerOutputData = hrsPerOutputData;
-        this.tempMethods = tempMethods;
-        this.merStrategy = merStrategy;
-        this.projectSDate = projectSDate;
-        this.calStrategy = calStrategy;
-        this.mergMethods = mergMethods;
-    }
-
-    /**
-     * <p>Use this when you want to send data to a ZonalSummaryCalculator object.</p>
-     * 
-     * @param inRaster - Type: File[] - A File object for each DataDate.<br/>
-     * Example:  <code>{@link #version2.prototype.DirectoryLayout.getIndexMetadata(ProjectInfo, String, DataDate, String)}<br/>
-     * getIndexMetadata(mProject, mIndex, sDate, zone.getShapeFile())</code>
-     * @param inShape - Type: File - The layer/shape file.<br/>
-     * Example:  <code>File({@link #version2.prototype.DirectoryLayout.getSettingsDirectory(ProjectInfo)},
-     * {@link #version2.prototype.ZonalSummary.getShapeFile()})<br/>
-     * File(DirectoryLayout.getSettingsDirectory(mProject), zone.getShapeFile())</code>
-     * @param outTable - Type: File - File object pointing to output location for zonal summary
-     * @param zone - Type: String - The zone name.<br/>
-     * Example:  <code>for ({@link version2.prototype.ZonalSummary ZonalSummary} zone : mProject.{@link #version2.prototype.ProjectInfo.getSummaries()}) { zone.{@link #version2.prototype.ZonalSummary.getField()}; }<br/>
-     * for (ZonalSummary zone : mProject.getSummaries())<br/>  { zone.getField(); }</code>
-     * @param summarySingletonNames - Type: ArrayList<String> - A list of the class names of the summaries to use in zonal summary.<br/>
-     * @throws ClassNotFoundException
-     * @throws NoSuchMethodException
-     * @throws SecurityException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * @throws IllegalArgumentException
-     * @throws InvocationTargetException
-     */
-    public SummaryData(File inRaster, File inShape, File outTable, String zone, ArrayList<String> summarySingletonNames)
-            throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException,
-            IllegalAccessException, IllegalArgumentException, InvocationTargetException{
-        this.inRaster = inRaster;
-        this.inShape = inShape;
-        outTableFile = outTable;
-        zoneField = zone;
-        summaries = new SummariesCollection(summarySingletonNames);
-
-        inDate = null;
-        hrsPerInputData = -1;
-        hrsPerOutputData = -1;
-        tempMethods = null;
-        merStrategy = null;
-        projectSDate = null;
-        calStrategy = null;
-        mergMethods = null;
-    }
-
-    /**
-     * <p>Use this when you want to send data to a TemporalSummaryCalculator object.</p>
-     * 
-     * @param inRaster - Type: File[] - A File object for each DataDate.<br/>
-     * Example:  <code>{@link #version2.prototype.DirectoryLayout.getIndexMetadata(ProjectInfo, String, DataDate, String)}<br/>
-     * getIndexMetadata(mProject, mIndex, sDate, zone.getShapeFile())</code>
-     * @param inShape - Type: File - The layer/shape file.<br/>
-     * Example:  <code>File({@link #version2.prototype.DirectoryLayout.getSettingsDirectory(ProjectInfo)},
-     * {@link #version2.prototype.ZonalSummary.getShapeFile()})<br/>
-     * File(DirectoryLayout.getSettingsDirectory(mProject), zone.getShapeFile())</code>
-     * @param outTable - Type: File - File object pointing to output location for zonal summary
-     * Example:  <code>for ({@link version2.prototype.ZonalSummary ZonalSummary} zone : mProject.{@link #version2.prototype.ProjectInfo.getSummaries()}) { zone.{@link #version2.prototype.ZonalSummary.getField()}; }<br/>
-     * for (ZonalSummary zone : mProject.getSummaries())<br/>  { zone.getField(); }</code>
-     * @param inDate - Type: DataDate[] - An array of the dates of the downloaded data to be used in finding the data in the file system and in processing temporal summaries.<br/>
-     * @param hrsPerInputData - Type: int - The number of hours each piece of downloaded data represents.
-     * @param hrsPerOutputData - Type: int - The number of hours each piece of summary/output data will represent.
-     * @param projectSDate - Type: Calendar - The projects start date.
-     * @param calStrategy - Type: CalendarStrategy - The strategy to use when getting the starting date of the week.
-     * @param merStrategy - Type: MergeStrategy - The strategy to use when merging downloaded data.
-     * @param tempMethods - Type: ArrayList<TemporalSummary> - The list of summary methods to calculate for temporal summary.
-     * @param mergMethods - Type: ArrayList<MergeSummary> - The list of summary methods to use during merging with the chosen MergeStrategy.
-     * @throws ClassNotFoundException
-     * @throws NoSuchMethodException
-     * @throws SecurityException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * @throws IllegalArgumentException
-     * @throws InvocationTargetException
-     */
-    public SummaryData(File inRaster, File inShape, File outTable, DataDate[] inDate, int hrsPerInputData,
-            int hrsPerOutputData, Calendar projectSDate, CalendarStrategy calStrategy,
-            MergeStrategy merStrategy, ArrayList<TemporalSummary> tempMethods,
-            ArrayList<MergeSummary> mergMethods) throws ClassNotFoundException, NoSuchMethodException,
-            SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException,
-            InvocationTargetException {
-        this.inRaster = inRaster;
-        this.inShape = inShape;
-        outTableFile = outTable;
-        zoneField = null;
-        summaries = null;
-        this.inDate = inDate;
-        this.hrsPerInputData = hrsPerInputData;
-        this.hrsPerOutputData = hrsPerOutputData;
+        this.hrsPerInputData = hrsPerInputData;     // set to -1 when not used
+        this.hrsPerOutputData = hrsPerOutputData;   // set to -1 when not used
         this.tempMethods = tempMethods;
         this.merStrategy = merStrategy;
         this.projectSDate = projectSDate;
